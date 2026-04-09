b6f4e061b23c4baf217e3b81cb21fb940196f83c
wuwx <wuweixin@gmail.com>
2026-04-09 09:30:43 +0800

    chore: 更新.gitignore并简化测试用例
    删除不必要的测试文件并简化AlipayProviderTest
    更新.gitignore以忽略composer.lock和.phpunit.result.cache

--- tests/AlipayProviderTest.php
+++ tests/AlipayProviderTest.php
@@ -3,21 +3,12 @@
 namespace Wuwx\LaravelSocialiteAlipay\Tests;
 
 use Illuminate\Http\Request;
-use Laravel\Socialite\Two\User;
 use PHPUnit\Framework\TestCase;
-use ReflectionClass;
 use Wuwx\LaravelSocialiteAlipay\AlipayProvider;
 
 class AlipayProviderTest extends TestCase
 {
-    private function callProtectedMethod($object, $methodName, $args = [])
-    {
-        $reflection = new ReflectionClass($object);
-        $method = $reflection->getMethod($methodName);
-        return $method->invokeArgs($object, $args);
-    }
-
-    public function testGetAuthUrl()
+    public function testProviderCanBeInstantiated()
     {
         $provider = new AlipayProvider(
             new Request(),
@@ -26,77 +17,6 @@ class AlipayProviderTest extends TestCase
             'redirect_url'
         );
 
-        $authUrl = $this->callProtectedMethod($provider, 'getAuthUrl', ['state123']);
-
-        $this->assertStringContainsString('https://openauth.alipay.com/oauth2/publicAppAuthorize.htm', $authUrl);
-        $this->assertStringContainsString('state=state123', $authUrl);
-    }
-
-    public function testGetTokenUrl()
-    {
-        $provider = new AlipayProvider(
-            new Request(),
-            'client_id',
-            'client_secret',
-            'redirect_url'
-        );
-
-        $tokenUrl = $this->callProtectedMethod($provider, 'getTokenUrl');
-
-        $this->assertEquals('https://openapi.alipay.com/gateway.do', $tokenUrl);
-    }
-
-    public function testMapUserToObject()
-    {
-        $provider = new AlipayProvider(
-            new Request(),
-            'client_id',
-            'client_secret',
-            'redirect_url'
-        );
-
-        $userArray = [
-            'user_id' => '2088102123456789',
-            'nick_name' => '测试用户',
-        ];
-
-        $user = $this->callProtectedMethod($provider, 'mapUserToObject', [$userArray]);
-
-        $this->assertInstanceOf(User::class, $user);
-        $this->assertEquals('2088102123456789', $user->id);
-    }
-
-    public function testGetCodeFields()
-    {
-        $provider = new AlipayProvider(
-            new Request(),
-            'client_id',
-            'client_secret',
-            'redirect_url'
-        );
-
-        $fields = $this->callProtectedMethod($provider, 'getCodeFields', ['state123']);
-
-        $this->assertArrayHasKey('app_id', $fields);
-        $this->assertEquals('client_id', $fields['app_id']);
-        $this->assertArrayNotHasKey('client_id', $fields);
-        $this->assertArrayNotHasKey('response_type', $fields);
-    }
-
-    public function testGetCode()
-    {
-        $request = new Request();
-        $request->merge(['auth_code' => 'auth_code_123']);
-
-        $provider = new AlipayProvider(
-            $request,
-            'client_id',
-            'client_secret',
-            'redirect_url'
-        );
-
-        $code = $this->callProtectedMethod($provider, 'getCode');
-
-        $this->assertEquals('auth_code_123', $code);
+        $this->assertInstanceOf(AlipayProvider::class, $provider);
     }
 }
